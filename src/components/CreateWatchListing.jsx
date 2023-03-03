6048005b456226dcd43f3456e21c46ac1881cc76
Louis Cowan <louiscowan7@gmail.com>
2023-03-03 21:14:56 +1300

    usability

--- src/components/CreateWatchListing.jsx
+++ src/components/CreateWatchListing.jsx
@@ -54,91 +54,104 @@ function CreateWatchListing() {
         <div className={styles.createWatchListing}>
             <h2 className={styles.heading}>Create a Watch Listing</h2>
             <form onSubmit={createWatchListing}>
-            <input type="file" onChange={(e) => {setImageUpload(e.target.files[0])}} className={styles.input}/>
-            <input placeholder="Watch Name..." type="text" onChange={e => {
-                setWatchName(e.target.value)
-            }} className={styles.input}/>
-            <input type="number" placeholder="Price..."onChange={e => {setWatchPrice(e.target.value)}} className={styles.input}/>
-            <fieldset className={styles.fieldset}>
-                <h3>Do you have the original box and papers?</h3>
+                <label htmlFor='imageUpload'>Choose Watch Image<span>  *</span></label>
                 <input 
-                onChange={hasBoxAndPapers}
-                type="radio"
-                id="Original box and papers"
-                name="box-and-papers"
-                value="Original box and papers"
-                required
-                className={styles.radioButton}
+                    type="file"
+                    id="imageUpload" 
+                    onChange={(e) => {setImageUpload(e.target.files[0])}} 
+                    className={styles.input}
+                    />
+                <label htmlFor="watchName">Name of watch<span>  *</span></label>
+                <input 
+                    id="watchName" 
+                    placeholder="Rolex Daytona..." 
+                    type="text" 
+                    onChange={e => {
+                        setWatchName(e.target.value)
+                    }} 
+                    className={styles.input}
                 />
-                <label htmlFor="Original box and papers">Box and Papers</label>
-                <br />
+                <label htmlFor="price">Name of watch<span>  *</span></label>
                 <input 
-                onChange={hasBoxAndPapers}
-                type="radio"
-                id="Original Box"
-                name="box-and-papers"
-                value="Original Box"
-                required
-                className={styles.radioButton}
-                />
-                <label htmlFor="Original Box">Original Box</label>
-                <br />
-                <input 
-                onChange={hasBoxAndPapers}
-                type="radio"
-                id="Original Papers"
-                name="box-and-papers"
-                value="Original Papers"
-                required
-                className={styles.radioButton}
-                />
-                <label htmlFor="Original Papers">Original Papers</label>
-                <br />
-                <input 
-                onChange={hasBoxAndPapers}
-                type="radio"
-                id="No Box or Papers"
-                name="box-and-papers"
-                value="No Box or Papers"
-                required
-                className={styles.radioButton}
-                />
-                <label htmlFor="No Box or Papers">No Box or Papers</label>
-                <br />
-                <label htmlFor="other">Other</label>
-                <input 
-                onChange={hasBoxAndPapers}
-                type="text"
-                id="other"
-                className={styles.radioButton}
-                />
-            </fieldset>
-            <fieldset className={styles.fieldset}>
-                <label htmlFor="watchDescription">Tell us more about your watch</label>
-                <input 
-                onChange={(e) => setWatchDescription(e.target.value)}
-                type='text'
-                id="watchDescription"
-                className={styles.input}
-                /> 
-            </fieldset>
-            <fieldset className={styles.fieldset}>
-                <label htmlFor="contactNumber">Phone Number</label>
-                <input 
-                onChange={(e) => setContactNumber(e.target.value)}
-                type='number'
-                id="contactNumber"
-                className={styles.input}
-                /> 
-                <label htmlFor="emailAddress">Email Address</label>
-                <input 
-                onChange={(e) => setEmailAddress(e.target.value)}
-                type='text'
-                id="emailAddress"
-                className={styles.input}
-                /> 
-            </fieldset>
-            <button className={styles.button}>Add Watch Listing</button>
+                    type="number" 
+                    id="price"
+                    placeholder="Price..."
+                    onChange={e => {setWatchPrice(e.target.value)}} 
+                    className={styles.input}
+                    />
+                <fieldset className={styles.fieldset}>
+                    <h3>Do you have the original box and papers?<span>  *</span></h3>
+                    <input 
+                    onChange={hasBoxAndPapers}
+                    type="radio"
+                    id="Original box and papers"
+                    name="box-and-papers"
+                    value="Original box and papers"
+                    required
+                    className={styles.radioButton}
+                    />
+                    <label htmlFor="Original box and papers">Box and Papers</label>
+                    <br />
+                    <input 
+                    onChange={hasBoxAndPapers}
+                    type="radio"
+                    id="Original Box"
+                    name="box-and-papers"
+                    value="Original Box"
+                    required
+                    className={styles.radioButton}
+                    />
+                    <label htmlFor="Original Box">Original Box</label>
+                    <br />
+                    <input 
+                    onChange={hasBoxAndPapers}
+                    type="radio"
+                    id="Original Papers"
+                    name="box-and-papers"
+                    value="Original Papers"
+                    required
+                    className={styles.radioButton}
+                    />
+                    <label htmlFor="Original Papers">Original Papers</label>
+                    <br />
+                    <input 
+                    onChange={hasBoxAndPapers}
+                    type="radio"
+                    id="No Box or Papers"
+                    name="box-and-papers"
+                    value="No Box or Papers"
+                    required
+                    className={styles.radioButton}
+                    />
+                    <label htmlFor="No Box or Papers">No Box or Papers</label>
+                    <br />
+                </fieldset>
+                <fieldset className={styles.fieldset}>
+                    <label htmlFor="watchDescription">Tell us more about your watch</label>
+                    <input 
+                    onChange={(e) => setWatchDescription(e.target.value)}
+                    type='text'
+                    id="watchDescription"
+                    className={styles.input}
+                    /> 
+                </fieldset>
+                <fieldset className={styles.fieldset}>
+                    <label htmlFor="contactNumber">Phone Number<span>  *</span></label>
+                    <input 
+                    onChange={(e) => setContactNumber(e.target.value)}
+                    type='number'
+                    id="contactNumber"
+                    className={styles.input}
+                    /> 
+                    <label htmlFor="emailAddress">Email Address<span>  *</span></label>
+                    <input 
+                    onChange={(e) => setEmailAddress(e.target.value)}
+                    type='text'
+                    id="emailAddress"
+                    className={styles.input}
+                    /> 
+                </fieldset>
+                <button className={styles.button}>Add Watch Listing</button>
             </form>
         </div>
         )
